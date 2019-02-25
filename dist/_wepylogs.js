069a2f8490bf8accfee740747976b07d98793818
calinas <1607731780@qq.com>
2019-02-25 18:07:38 +0800

    update

--- dist/_wepylogs.js
+++ dist/_wepylogs.js
@@ -1,2 +1,4 @@
 console.log('WePY开启错误监控');
-console.warn("CLI报错：WARNING IN : src\pages\bindRelationship.wpy\n[xmldom warning]	unclosed xml attribute\n@#[line:17,col:11]");
+console.warn("CLI报错：WARNING IN : src\components\modal.wpy\n[xmldom warning]	attribute \"{{placeholderText}}\" missed quot(\")!!\n@#[line:5,col:9]");
+console.warn("CLI报错：WARNING IN : src\components\modal.wpy\n[xmldom warning]	unclosed xml attribute\n@#[line:5,col:9]");
+console.warn("CLI报错：WARNING IN : src\components\searchResult.wpy\n[xmldom warning]	unclosed xml attribute\n@#[line:6,col:9]");
