3c80e4af9cf4cf7e82aef10823909183d40874d7
Calinas <1607731780@qq.com>
2019-02-26 22:58:20 +0800

    修复

--- dist/_wepylogs.js
+++ dist/_wepylogs.js
@@ -1,2 +1,2 @@
 console.log('WePY开启错误监控');
-console.warn("CLI报错：WARNING IN : src\components\commentModal.wpy\n[xmldom warning]	attribute \"{{placeholderText}}\" missed quot(\")!!\n@#[line:5,col:9]");
+console.warn("CLI报错：WARNING IN : src\pages\bindRelationship.wpy\n[xmldom warning]	unclosed xml attribute\n@#[line:17,col:11]");
