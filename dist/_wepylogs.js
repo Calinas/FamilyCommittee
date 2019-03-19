5e03c5c40a184089bb58c91abbd2e453d78c4a11
calinas <1607731780@qq.com>
2019-03-19 18:12:05 +0800

    代码提交

--- dist/_wepylogs.js
+++ dist/_wepylogs.js
@@ -1,8 +1,2 @@
 console.log('WePY开启错误监控');
-console.warn("CLI报错：WARNING IN : src\components\modal.wpy\n[xmldom warning]	attribute \"{{placeholderText}}\" missed quot(\")!!\n@#[line:5,col:9]");
-console.warn("CLI报错：WARNING IN : src\components\modal.wpy\n[xmldom warning]	attribute \"{{placeholderText}}\" missed quot(\")!!\n@#[line:5,col:9]");
-console.warn("CLI报错：WARNING IN : src\components\searchResult.wpy\n[xmldom warning]	unclosed xml attribute\n@#[line:6,col:9]");
-console.warn("CLI报错：WARNING IN : src\components\modal.wpy\n[xmldom warning]	attribute \"{{placeholderText}}\" missed quot(\")!!\n@#[line:5,col:9]");
-console.warn("CLI报错：WARNING IN : src\components\modal2.wpy\n[xmldom warning]	attribute \"{{placeholderText}}\" missed quot(\")!!\n@#[line:5,col:9]");
-console.warn("CLI报错：WARNING IN : src\components\modal2.wpy\n[xmldom warning]	unclosed xml attribute\n@#[line:5,col:9]");
-console.warn("CLI报错：WARNING IN : src\components\modal2.wpy\n[xmldom warning]	attribute \"{{placeholderText2}}\" missed quot(\")!!\n@#[line:6,col:9]");
+console.warn("CLI报错：WARNING IN : src\pages\photos.wpy\n[xmldom warning]	unclosed xml attribute\n@#[line:7,col:13]");
