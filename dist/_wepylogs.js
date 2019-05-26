5ffeffac2bbf4859708c422a299d7708bdddddaf
calinas <1607731780@qq.com>
2019-05-26 18:45:38 +0800

    希望这是最后一次提交

--- dist/_wepylogs.js
+++ dist/_wepylogs.js
@@ -1,3 +1,1 @@
 console.log('WePY开启错误监控');
-console.warn("CLI报错：WARNING IN : src\components\commentModal.wpy\n[xmldom warning]	attribute \"{{placeholderText}}\" missed quot(\")!!\n@#[line:6,col:9]");
-console.warn("CLI报错：WARNING IN : src\components\commentModal.wpy\n[xmldom warning]	attribute \"{{placeholderText}}\" missed quot(\")!!\n@#[line:6,col:9]");
